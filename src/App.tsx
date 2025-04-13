acbe56686ad451ebfc4126674714b8852809c876
Shivam Singh <shivam@technoscore.net>
2025-04-13 23:09:37 +0530

    Added initial basic setup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import siteLogo from '/logo-bl-t.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={siteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1 className='font-face-logo'>HOUSE OF LOOKS</h1>
+      <div className="card">
+        
+      </div>
+      <p className="read-the-docs">
+        SAYA FASHION LLP © 2025
+      </p>
+    </>
+  )
+}
+
+export default App
