54a3ac0e8b57d86ab842f7c9f60f186b88f4a95e
Shivam Singh <shivam@technoscore.net>
2025-05-17 07:48:44 +0530

    updates in images path

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 // import axios from "axios";
 // import { useEffect } from "react";
+import forHerImage from "../assets/images/for_her.jfif";
+import forHimImage from "../assets/images/for-him.jpg";
 
 const Home = () => {
   // useEffect(() => {
@@ -13,21 +15,21 @@ const Home = () => {
   return (
     <>
       <div className="carousel carousel-center bg-neutral rounded-box space-x-4 p-4">
-        <div className="carousel-item bg-[url('./src/assets/images/443900463_1497658181105760_4024221241233185618_n.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('./src/assets/images/tshirt.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title text-amber-100 justify-end uppercase">
               <span className="bg-black p-1 rounded-box">Tshirts</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/jeans-428614_1280.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('./src/assets/images/jeans.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end text-amber-100 uppercase">
               <span className="bg-black p-1 rounded-box">Jeans</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/469806188_122152731050289777_1161863373712766166_n.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('./src/assets/images/cargo.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase text-amber-100">
               <span className="bg-black p-1 rounded-box">Cargos</span>
@@ -41,21 +43,21 @@ const Home = () => {
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/454236377_1025698259207159_1465278236567567516_n.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('./src/assets/images/co-ord.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase  text-amber-100">
               <span className="bg-black p-1 rounded-box">Co-Ords</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/455264203_1033709845068951_8863746397986071115_n.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('./src/assets/images/suit.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase text-amber-900">
               <span className="bg-black p-1 rounded-box">Suits</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/444990111_412716188294508_5175143283363269065_n.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('./src/assets/images/shoes.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase text-amber-100">
               <span className="bg-black p-1 rounded-box">Shoes</span>
@@ -66,12 +68,7 @@ const Home = () => {
       <div className="flex justify-center items-center gap-2 md:w-[50%] mx-auto ">
         <div className="card bg-base-100 image-full shadow-sm mt-4">
           <figure>
-            <img
-              src="./src/assets/images/man-5693573_1280.jpg"
-              alt="Shoes"
-              height="300"
-              width="300"
-            />
+            <img src={forHimImage} alt="Shoes" height="300" width="300" />
           </figure>
           <div className="card-body flex  flex-col justify-center">
             <h2 className="card-title justify-center">For Him</h2>
@@ -79,12 +76,7 @@ const Home = () => {
         </div>
         <div className="card bg-base-100 image-full shadow-sm mt-4">
           <figure>
-            <img
-              src="./src/assets/images/for_her.jfif"
-              alt="Shoes"
-              height="300"
-              width="300"
-            />
+            <img src={forHerImage} alt="Shoes" height="300" width="300" />
           </figure>
           <div className="card-body flex flex-col justify-center">
             <h2 className="card-title justify-center">For Her</h2>
