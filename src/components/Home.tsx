8624c73f1b96aa40392e253ee3025b3e8d00fbaa
Shivam Singh <shivam@technoscore.net>
2025-05-17 07:56:07 +0530

    updates

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,49 +15,49 @@ const Home = () => {
   return (
     <>
       <div className="carousel carousel-center bg-neutral rounded-box space-x-4 p-4">
-        <div className="carousel-item bg-[url('./src/assets/images/tshirt.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/tshirt.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title text-amber-100 justify-end uppercase">
               <span className="bg-black p-1 rounded-box">Tshirts</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/jeans.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/jeans.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end text-amber-100 uppercase">
               <span className="bg-black p-1 rounded-box">Jeans</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/cargo.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/cargo.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase text-amber-100">
               <span className="bg-black p-1 rounded-box">Cargos</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/kurta.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/kurta.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase text-amber-100">
               <span className="bg-black p-1 rounded-box">Kurtas</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/co-ord.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/co-ord.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase  text-amber-100">
               <span className="bg-black p-1 rounded-box">Co-Ords</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/suit.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/suit.jfif')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
-            <h2 className="card-title justify-end uppercase text-amber-900">
+            <h2 className="card-title justify-end uppercase text-amber-100">
               <span className="bg-black p-1 rounded-box">Suits</span>
             </h2>
           </div>
         </div>
-        <div className="carousel-item bg-[url('./src/assets/images/shoes.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
+        <div className="carousel-item bg-[url('/src/assets/images/shoes.jpg')] w-[300px] h-[300px] rounded-box bg-cover">
           <div className="card-body flex ">
             <h2 className="card-title justify-end uppercase text-amber-100">
               <span className="bg-black p-1 rounded-box">Shoes</span>
